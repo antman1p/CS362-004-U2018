21d04ec554740a99cb11dfdc9ef9af8b002ab6f8
Piazza <piazzaan@oregonstate.edu>
2018-07-08 14:23:16 -0400

    Added Bugs 1

--- projects/piazzaan/dominion/dominion.c
+++ projects/piazzaan/dominion/dominion.c
@@ -1295,23 +1295,23 @@ void adventurerFunc(struct gameState *state, int currentPlayer, int temphand[], 
 {
 	int cardDrawn;
 	int drawntreasure = 0;
-	while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
+	while(drawntreasure<=2){
+		if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+		  shuffle(currentPlayer, state);
+		}
+		drawCard(currentPlayer, state);
+		cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+		if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+		  drawntreasure++;
+		else{
+		  temphand[z]=cardDrawn;
+		  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+		  z++;
+		}
       }
       while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
+		state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+		z=z-1;
       }
 }
 
@@ -1319,7 +1319,7 @@ void smithyFunc(struct gameState *state, int currentPlayer, int handPos)
 {
 	int i;
 	//+3 Cards
-      for (i = 0; i < 3; i++)
+    for (i = 1; i < 3; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
@@ -1334,7 +1334,7 @@ void great_hallFunc(struct gameState *state, int currentPlayer, int handPos)
       drawCard(currentPlayer, state);
 			
       //+1 Actions
-      state->numActions++;
+      state->numActions--;
 			
       //discard card from hand
       discardCard(handPos, currentPlayer, state, 0);
