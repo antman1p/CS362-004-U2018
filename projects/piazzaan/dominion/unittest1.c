1150ddf1b1610081bd1148946887104febe376a8
Piazza <piazzaan@oregonstate.edu>
2018-07-13 18:03:14 -0400

    Created unittest1.c

---
+++ projects/piazzaan/dominion/unittest1.c
@@ -0,0 +1,18 @@
+/*
+ * unittest1.c
+ * CS 362_400
+ * Antonio Piazza
+ * piazzaan
+ * 7/15/2108
+ * 
+ * Unit test 1 for Dominion function ????
+ * 
+ */
+
+#include "dominion.h"
+
+
+// Variable to keep count of test fails
+int failCnt = 0;
+
+// 
