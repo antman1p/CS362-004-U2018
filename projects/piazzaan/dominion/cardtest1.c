2d00f628e959ae6f12762ca10b98197bfde2641b
Piazza <piazzaan@oregonstate.edu>
2018-07-15 12:19:17 -0400

    cardtest1.c debug

--- projects/piazzaan/dominion/cardtest1.c
+++ projects/piazzaan/dominion/cardtest1.c
@@ -130,7 +130,7 @@ int failCnt = 0;
 	// Test Kingdom Cards
 	printf("\nTest Kingdom card piles were not effected\n");
 	for (i = 0; i < 10; i++) {
-        printf("checking %s card pile\n", k[i]);
+        printf("checking %d card pile\n", k[i]);
 		printf("Expected: %d\n", copyGState.supplyCount[k[i]]);
 		printf("Result: %d\n", gState.supplyCount[k[i]]);
         assertTrue(gState.supplyCount[k[i]],copyGState.supplyCount[k[i]]);
