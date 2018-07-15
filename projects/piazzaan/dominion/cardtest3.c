209ca2ea202447fee5a8abe90106919d2feb9fa2
Piazza <piazzaan@oregonstate.edu>
2018-07-15 15:33:47 -0400

    cardtest3.c debug

--- projects/piazzaan/dominion/cardtest3.c
+++ projects/piazzaan/dominion/cardtest3.c
@@ -60,6 +60,9 @@ int failCnt = 0;
 	int k[10] = {adventurer, council_room, feast, gardens, mine
 		, remodel, smithy, village, outpost, great_hall};
 		
+	const char* cardNames[10] = {"adventurer", "council_room", "feast", "gardens", "mine"
+		, "remodel", "smithy", "village", "outpost", "great_hall"};
+		
 	// Initialize two game states
 	struct gameState gState, copyGState;
 
@@ -130,7 +133,7 @@ int failCnt = 0;
 	// Test Kingdom Cards
 	printf("\nTest Kingdom card piles were not effected\n");
 	for (i = 0; i < 10; i++) {
-        printf("\nchecking %d card pile\n", k[i]);
+        printf("\nchecking %s card pile\n", cardNames[k[i]]);
 		printf("Expected: %d\n", copyGState.supplyCount[k[i]]);
 		printf("Result: %d\n", gState.supplyCount[k[i]]);
         assertTrue(gState.supplyCount[k[i]],copyGState.supplyCount[k[i]]);
