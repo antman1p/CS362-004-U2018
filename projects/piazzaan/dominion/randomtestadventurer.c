c2aff87b9ef91a5538fd8ff6db89983af0a9d0ce
Piazza <piazzaan@oregonstate.edu>
2018-08-04 11:08:54 -0400

    Random Adventurere Test debug

--- projects/piazzaan/dominion/randomtestadventurer.c
+++ projects/piazzaan/dominion/randomtestadventurer.c
@@ -53,7 +53,7 @@ int failCnt = 0;
 	
 	
 	// randomly select player position number
-	playerNum = rand() % (MAX_PLAYERS + 1 - 1) + 1
+	playerNum = rand() % (MAX_PLAYERS + 1 - 1) + 1;
 	
 	//debug
 	printf("playerNum = %d\n", playerNum);
