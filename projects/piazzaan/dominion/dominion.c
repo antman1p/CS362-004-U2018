0e266db2dbdb5bfada7bd484a82b754b0bd471d1
Piazza <piazzaan@oregonstate.edu>
2018-08-06 18:55:31 -0400

    Added bugs back

--- projects/piazzaan/dominion/dominion.c
+++ projects/piazzaan/dominion/dominion.c
@@ -1295,7 +1295,7 @@ void adventurerFunc(struct gameState *state, int currentPlayer, int temphand[], 
 {
 	int cardDrawn;
 	int drawntreasure = 0;
-	while(drawntreasure<2){
+	while(drawntreasure<3){
 		if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 		  shuffle(currentPlayer, state);
 		}
@@ -1319,7 +1319,7 @@ void smithyFunc(struct gameState *state, int currentPlayer, int handPos)
 {
 	int i;
 	//+3 Cards
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 2; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
