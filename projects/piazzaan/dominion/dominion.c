f1709be94d9ead5f1efe758eb784a020c7aaf72f
Piazza <piazzaan@oregonstate.edu>
2018-08-05 21:01:49 -0400

    random test card 1 debug

--- projects/piazzaan/dominion/dominion.c
+++ projects/piazzaan/dominion/dominion.c
@@ -1319,7 +1319,7 @@ void smithyFunc(struct gameState *state, int currentPlayer, int handPos)
 {
 	int i;
 	//+3 Cards
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < 3; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
